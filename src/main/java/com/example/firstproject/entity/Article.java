5932ec83e7fc7934e0a78138eef7d4132270c73f
배규리 <77094833+softwarerbfl@users.noreply.github.com>
2022-01-06 23:39:21 +0900

    Update Article.java

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,8 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
 @Entity //DB가 해당 객체를 인식 가능
 @AllArgsConstructor
 @ToString
+@NoArgsConstructor //디폴트 생성자 만들어주는 어노테이션
+@Getter //Article객체의 id return해주는 getId()함수 사용 가능
 public class Article {
 
     @Id //대표값을 지정! like 주민등록번호
@@ -21,5 +25,6 @@ public class Article {
     private String title;
     @Column
     private String content;
+
 
 }
