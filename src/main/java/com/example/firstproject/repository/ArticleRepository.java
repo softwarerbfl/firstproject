a325f2b683f1f175094411b89c5a9b2e0aa2e3d1
배규리 <77094833+softwarerbfl@users.noreply.github.com>
2022-01-06 23:39:40 +0900

    Update ArticleRepository.java

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -2,7 +2,11 @@ package com.example.firstproject.repository;
 
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
 //자바 내에 선언되어있는 repository 상속받아 사용
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
+    @Override
+    ArrayList<Article> findAll();
 }
