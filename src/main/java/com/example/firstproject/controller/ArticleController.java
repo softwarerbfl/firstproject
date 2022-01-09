27791c2615abcbb9f3a1e00d59fbee7c78f7a92a
softwarerbfl <77094833+softwarerbfl@users.noreply.github.com>
2022-01-10 03:08:45 +0900

    15 edit data

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -101,6 +103,24 @@ public class ArticleController {
 
         //3 : 수정 결과 페이지로 리다이렉트
         return "redirect:/articles/"+articleEntity.getId();
+    }
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable long id, RedirectAttributes rttr){ //RedirectAttributes는 삭제가 이루어졌을 때 사용된다.
+        log.info("삭제 요청이 들어왔습니다!!");
+
+        // 1 : 삭제 대상을 가져온다
+        Article target =articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 2 : 대상을 삭제한다!
+        if(target!=null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제가 완료되었습니다");
+            //redirect된 페이지에서 사용되는 휘발성 데이터
+        }
+
+        // 3 : 결과 페이지로 리다이렉트한다!
+        return "redirect:/articles";
     }
 }
 
